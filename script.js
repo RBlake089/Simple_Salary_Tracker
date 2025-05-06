f6558731586d97eb8ce6ebc0e4a3e239badf792c
Robert Blake <staton089@gmail.com>
2025-05-06 00:54:01 -0400

    fix function

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-addBtnEl = document.getElementById("add-btn-el");
-dateEl = document.getElementById("date");
-salaryEl = document.getElementById("salary");
-dateAndSalaryEl = document.getElementById("date-salary-el");
+// DOM Elements
+const addBtnEl = document.getElementById("add-btn-el");
+const dateEl = document.getElementById("date");
+const salaryEl = document.getElementById("salary");
+const dateAndSalaryEl = document.getElementById("date-salary-el");
 
-
-//FUNCTIONS 
-addBtnEl.eventListener("click", function() {
-    
-
+// FUNCTIONS 
+addBtnEl.addEventListener("click", function() {
+    // Your code here
 });
