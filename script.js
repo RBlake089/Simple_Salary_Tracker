5de5f322142896a86bd5a52495234ab0993641bd
Robert Blake <staton089@gmail.com>
2025-05-06 00:52:17 -0400

    working on JS function

---
+++ script.js
@@ -0,0 +1,11 @@
+addBtnEl = document.getElementById("add-btn-el");
+dateEl = document.getElementById("date");
+salaryEl = document.getElementById("salary");
+dateAndSalaryEl = document.getElementById("date-salary-el");
+
+
+//FUNCTIONS 
+addBtnEl.eventListener("click", function() {
+    
+
+});
