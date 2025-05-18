d30cc1d4eb422bfce237a8f40ce63221694b3712
Robert Blake <staton089@gmail.com>
2025-05-17 20:16:54 -0400

    updated htrml and css and js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // DOM Elements
 const addBtnEl = document.getElementById("add-btn-el");
-const dateEl = document.getElementById("date");
-const salaryEl = document.getElementById("salary");
+const dateEl = document.getElementById("date-el");
+const salaryEl = document.getElementById("salary-el");
 const dateAndSalaryEl = document.getElementById("date-salary-el");
+const dateSalaryEl = document.getElementById("date-salary-el")
 
 // FUNCTIONS 
 addBtnEl.addEventListener("click", function() {
